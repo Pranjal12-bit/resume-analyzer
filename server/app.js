e2fcda8410545c21fa5e19760dc5c20f1c60c76d
Pranjal12-bit <134685605+Pranjal12-bit@users.noreply.github.com>
2025-07-12 18:52:22 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -49,7 +49,7 @@ connectDatabase();
 
 const allowedOrigins = [
   "http://localhost:3000", // Local development frontend
-  "https://resume-analyzer-backend-e7xk.onrender.com", // Production frontend
+  "https://resume-analyzer-frontend-j7zp.onrender.com"// Production frontend
 ];
 
 const corsOptions = {
